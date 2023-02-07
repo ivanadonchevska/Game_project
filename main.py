d5431893642cdd0ed82081c3dcd0e004bb4c12ca
Ivana Donchevska <ivana.donchevska77@gmail.com>
2023-02-07 20:49:52 +0200

    Add player images

--- main.py
+++ main.py
@@ -9,14 +9,20 @@ pygame.display.set_caption("Game")
 
 x = 200
 y = 200
-
+scale = 3
+image = pygame.image.load("Images/Enemy/Idle/0 (1).png")
+image = pygame.transform.scale(image, (int(image.get_width() * scale), int(image.get_height() * scale)))
+rectangle = image.get_rect()
+rectangle.center = x, y
 
 
 run = True
 while run:
+    screen.blit(image, rectangle)
     for event in pygame.event.get():
         # quit game
         if event.type == pygame.QUIT:
             run = False
 
+    pygame.display.update()
 pygame.quit()
