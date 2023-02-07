2def6a28f2b58db6a2d8332aa8af127958716e62
Ivana Donchevska <ivana.donchevska77@gmail.com>
2023-02-07 20:32:04 +0200

    Add folder with images

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Game")
 
+x = 200
+y = 200
+
+
 
 run = True
 while run:
